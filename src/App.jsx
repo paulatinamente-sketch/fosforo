11e7758175a2afa29c762523cfb948f6c2347a8c
paulatinamente-sketch <paulatinamente-sketch@users.noreply.github.com>
2026-08-29 21:34:52 -0300

    Fix: restore fosforo project, remove extra files from other project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MatchCV from "./MatchCV.jsx";
-import DiagnosticoRRHH from "./DiagnosticoRRHH/DiagnosticoRRHH.jsx";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MatchCV />} />
-        <Route path="/diagnostico" element={<DiagnosticoRRHH />} />
-      </Routes>
+    <>
+      <MatchCV />
       <Analytics />
-    </BrowserRouter>
+    </>
   );
 }
